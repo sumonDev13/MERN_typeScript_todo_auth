46b423f6acddd100ed48f7427124b5e35c710000
sumonDev13 <sumonmondal1530765@gmail.com>
2023-11-04 16:29:09 +0600

    Initial Commit

---
+++ Backend/src/index.ts
@@ -0,0 +1,21 @@
+import express,{Application,Request,Response} from 'express';
+import { config } from 'dotenv';
+
+config()
+
+const app:Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req:Request, res:Response) => {
+    res.send('Api is running')
+})
+
+app.listen(port,()=>{
+    console.log(`typeScript server is listening on port ${port}`);
+});
+
+

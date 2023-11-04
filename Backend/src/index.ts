5ef588c08a1e81a3eaaa9fbf510990d85c790fee
sumonDev13 <sumonmondal1530765@gmail.com>
2023-11-04 21:25:10 +0600

    wip- index.js

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,9 @@
 import express,{Application,Request,Response} from 'express';
 import { config } from 'dotenv';
 import { dbConnection } from './config/databaseConnection';
+import userRouter from './routes/userRoute';
+import todoRouter from './routes/todoRoute';
+import notFound from './middleware/notFound';
 
 config()
 
@@ -16,6 +19,12 @@ dbConnection();
 app.get('/', (req:Request, res:Response) => {
     res.send('Api is running')
 })
+
+app.use("/api/v1/users", userRouter);
+
+app.use("/api/v1/todos", todoRouter);
+
+app.use(notFound);
 
 app.listen(port,()=>{
     console.log(`typeScript server is listening on port ${port}`);
